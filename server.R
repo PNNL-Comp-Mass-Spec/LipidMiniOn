2c2858bf2256a778967c4ea0dab1a351d41f117a
Kelly Stratton <kelly.stratton@pnnl.gov>
2018-01-19 15:23:57 -0800

    Work on upload tab; stub out tabs for enrichment analysis and visualization

--- server.R
+++ server.R
@@ -7,30 +7,134 @@
 #    http://shiny.rstudio.com/
 #
 
+# library(shiny)
+# 
+# # Define server logic required to draw a histogram
+# shinyServer(function(input, output) {
+#   
+#   # Observe click, update Visualize
+#   universeData <- eventReactive(input$click, {
+#     filename <- input$universe$datapath
+#     read.csv(filename)
+#   })
+#   
+#   enrichedData <- eventReactive(input$click, {
+#     filename <- input$enriched$datapath
+#     read.csv(filename)
+#   })
+#   
+#   
+#   # Display head of file
+#   output$head_universe <- renderDataTable({
+#     head(universeData())
+#   })
+#   
+# 
+#   # Display head of e_meta file
+#   output$head_enriched <- renderDataTable({
+#     head(enrichedData())
+#   })
+#   
+# })
+
+
+#options(shiny.maxRequestSize=30*1024^2, ch.dir = TRUE) 
 library(shiny)
+#library(rodin)
 
-# Define server logic required to draw a histogram
-shinyServer(function(input, output) {
+shinyServer(function(session, input, output){
   
-  # Observe click, update Visualize
-  rawData <- eventReactive(input$click, {
-    filename <- input$file1$datapath
-    read.csv(filename)
+  ######## Upload Tab ##############
+  #### Sidebar Panel ####
+  # Get data from files
+  universeData <-reactive({
+    req(input$universe$datapath)
+    filename <- input$universe$datapath
+    read.csv(filename, stringsAsFactors = FALSE)
   })
-  rawEmeta <- eventReactive(input$click, {
-    filename <- input$file_emeta$datapath
-    read.csv(filename)
-  })
-  
-  # Display head of file
-  output$head_file1 <- renderDataTable({
-    head(rawData())
+  enrichedData <- reactive({
+    req(input$enriched$datapath)
+    filename <- input$enriched$datapath
+    read.csv(filename, stringsAsFactors = FALSE)
   })
   
 
-  # Display head of e_meta file
-  output$head_emeta <- renderDataTable({
-    head(rawEmeta())
+  
+  #### Main Panel ####
+  # Display results
+  output$head_universe <- renderDataTable({
+    head(universeData())
+    }, 
+    options = list(dom = 't', searching = FALSE)
+  )
+  output$head_enriched <- renderDataTable({
+    head(enrichedData())
+    }, 
+    options = list(dom = 't', searching = FALSE)
+  )
+  output$num_universe <- renderText({
+    c('Number of lipids in Universe file: ', nrow(universeData()))
+  })
+  output$num_enriched <- renderText({
+    c('Number of lipids in Enriched file: ', nrow(enrichedData()))
+  })
+  
+  
+  #### Action Button Reactions ####
+  # Clean the 2 datasets #
+  clean <- eventReactive(input$process_click, {
+    validate(
+      #need(input$edata_id_col != 'Select one', 
+       #    'Please select a unique identifier column')
+      need(nrow(universeData()) > 0 & nrow(enrichedData()) > 0, 
+            'Please upload files with > 1 lipid')
+    )
+    
+    universeDataClean <- clean.lipid.list(universeData)
+    enrichedDataClean <- clean.lipid.list(enrichedData)
+    
+    # Display success message if everything is loaded correctly
+    output$process_success <- renderUI({
+      req(universeDataClean() & enrichedDataClean())
+      test1 <- head(universeDataClean())
+      test2 <- head(enrichedDataCl)
+      HTML('<h4 style= "color:#1A5276">Your data has been successfully uploaded. 
+           You may proceed to the subsequent tabs for analysis.</h4>')
+    })
+  }
+  )
+  
+  
+  
+  
+  
+  
+  ####### Preprocess Tab #######
+  output$tempplaceholder = renderText({"Summary of tests can go here"})
+  
+  
+  ####### Visualize Tab #######
+  #### Sidebar Panel ####
+  
+  # Which groups should be displayed (if multiple)? #
+  output$whichGroups1 <- renderUI({
+    selectInput('whichGroups1', 'Group 1', 
+                choices = sample_names(), 
+                multiple = TRUE)
+  })
+  output$whichGroups2 <- renderUI({
+    selectInput('whichGroups2', 'Group 2', 
+                choices = sample_names(), 
+                multiple = TRUE)
+  })
+  
+  #### Main Panel ####
+  # Display Kendrick or Van Krevelen plots
+  output$kendrick <- renderPlot({
+    
+  })
+  output$vankrev <- renderPlot({
+    
   })
   
 })
--- ui.R
+++ ui.R
@@ -7,37 +7,189 @@
 #    http://shiny.rstudio.com/
 #
 
+# library(shiny)
+# 
+# # Define UI for application that draws a histogram
+# shinyUI(fluidPage(
+# 
+#   # App title ----
+#   titlePanel("Lipid Mini-On: MINIng and ONtology"),
+# 
+#     # Main panel for displaying outputs ----
+#     mainPanel(
+# 
+#       # Output: Tabset w/ Upload and Visualize (graph) tabs
+#       tabsetPanel(type = "tabs",
+#                   
+#                   ### Upload Tab ###
+#                   tabPanel("Data Upload",
+#                            
+#                            # Universe File - character vector #
+#                            fileInput("universe", "Upload Universe File (.csv)",
+#                                      multiple = TRUE,
+#                                      accept = c("text/csv",
+#                                                 "text/comma-separated-values,text/plain",
+#                                                 ".csv")),
+#                            # tags$hr(),
+#                            
+#                            # Enriched File - character vector #
+#                            fileInput("enriched", "Upload Enriched File (.csv)",
+#                                      multiple = TRUE,
+#                                      accept = c("text/csv",
+#                                                 "text/comma-separated-values,text/plain",
+#                                                 ".csv")),
+#                            
+#                            # Action Button - process the files #
+#                            actionButton("click", "Process Data")
+#                            ),
+#                   
+#                   ### Statistical Tests Tab ###
+#                   tabPanel("Statistical Test(s)",
+#                            dataTableOutput("head_universe"),
+#                            dataTableOutput("head_enriched")
+#                            ),
+#                   
+#                   ### Visualize Tab ###
+#                   tabPanel("Visualize",
+#                            dataTableOutput("head_universe"),
+#                            dataTableOutput("head_enriched")
+#                            
+#                            )
+#                   )
+#                   )
+#   )
+# )
+
+
+
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+
 library(shiny)
 
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
-
+  
   # App title ----
   titlePanel("Lipid Mini-On: MINIng and ONtology"),
+  
+  # Main panel for displaying outputs ----
+  mainPanel(
+    
+    # Output: Tabset w/ Upload and Visualize (graph) tabs
+    tabsetPanel(type = "tabs",
+                
+                ################## Upload Panel ##################
+                tabPanel("Upload",
+                         sidebarLayout(
+                           
+                           sidebarPanel(
+                             
+                             ## Load Universe file ##
+                             fileInput("universe", "Upload 'Universe' Lipid Names (.csv)",
+                                       multiple = TRUE,
+                                       accept = c("text/csv",
+                                                  "text/comma-separated-values,text/plain",
+                                                  ".csv")),
+                             
+                             # Load Enriched file
+                             fileInput("enriched", "Upload 'Enriched' Lipid Names (.csv)",
+                                       multiple = TRUE,
+                                       accept = c("text/csv",
+                                                  "text/comma-separated-values,text/plain",
+                                                  ".csv")), 
+                             
+                             # Process data (clean it) #
+                             actionButton('process_click', 'Process Data')
+                           ), 
+                           
+                           ## Main Panel ##
+                           mainPanel(
+      
+                              textOutput("num_universe"), 
+                              dataTableOutput("head_universe"),
+                               
+                              textOutput("num_enriched"), 
+                              dataTableOutput("head_enriched"), 
+                             
+                              textOutput("process_success")
+                           )
+                          )),
 
-    # Main panel for displaying outputs ----
-    mainPanel(
 
-      # Output: Tabset w/ Upload and Visualize (graph) tabs
-      tabsetPanel(type = "tabs",
-                  tabPanel("Upload",
-                           fileInput("file1", "Upload Universe File (.csv)",
-                                     multiple = TRUE,
-                                     accept = c("text/csv",
-                                                "text/comma-separated-values,text/plain",
-                                                ".csv")),
-                           # tags$hr(),
-                           fileInput("file_emeta", "Upload Enriched File (.csv)",
-                                     multiple = TRUE,
-                                     accept = c("text/csv",
-                                                "text/comma-separated-values,text/plain",
-                                                ".csv")),
-                           actionButton('click', 'Load Files')),
-                  tabPanel("Visualize",
-                           dataTableOutput("head_file1"),
-                           dataTableOutput('head_emeta'))
-                  )
-                  )
-  )
+                ################## Preprocess Panel ##################
+                tabPanel("Enrichment Analysis",
+                         sidebarLayout(
+                           sidebarPanel(
+                             checkboxGroupInput('tests', 'I want to use the following:',
+                                                c("Fisher's Exact Test",
+                                                  "Binomial Test",
+                                                  "EASE score"),
+                                                selected = ''),
+                             
+
+                             selectInput("chooseplots", "To test",
+                                         choices = c("Intact" = 1,
+                                                     "Chain" = 2,
+                                                     "All Chains" = 3)
+                             ),
+                             
+                             actionButton("preprocess_click", "Process Data")
+
+
+                           ),
+                           mainPanel(# Add plots and visualization stuff here
+                             textOutput("tempplaceholder"),
+                             width = 7)
+
+
+                         )
+                ),
+                ################## Visualize Panel ##################
+                tabPanel("Visualize",
+                         sidebarLayout(
+                           sidebarPanel(
+                             selectInput('chooseplots', 'I want to plot a',
+                                         choices = c('Pie Chart' = 1,
+                                                     'Other?' = 2)
+                             )#,
+                             # selectInput('choose_single', 'Using a(n)',
+                             #             choices = c('Single sample' = 1, 'Overlay of multiple samples' = 2)),
+                             # conditionalPanel(
+                             #   condition = 'input.choose_single == 2',
+                             #   {fluidRow(
+                             #     column(6,
+                             #            uiOutput('whichGroups1')
+                             #     ),
+                             #     column(6,
+                             #            uiOutput('whichGroups2')
+                             #     )
+                             #   )})
+                           ),
+                           mainPanel(
+                             # conditionalPanel(
+                             #   condition = "input.chooseplots == 1",
+                             #   plotOutput('vankrev'),
+                             #   selectInput('placeholder_1', 'Use boundary:',
+                             #               choices = c('None' = 0, 'A' = 1, 'B' = 2, 'C' = 3))
+                             # ),
+                             conditionalPanel(
+                               condition = "input.chooseplots == 2",
+                               plotOutput('kendrick')
+                             ),
+                             selectInput('placeholder_1', 'Color by:',
+                                         choices = c('Placeholder 1' = 1, 'Placeholder 2' = 2)),
+
+                             width = 7)
+                         )
+                )
+    ))
+)
 )
 
