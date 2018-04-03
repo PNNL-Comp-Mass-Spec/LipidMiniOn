665f2ad1b9be5cfdc04fe6064a82b660d3d24972
Reehl, Sarah <sarah.reehl@pnnl.gov>
2018-04-03 10:46:15 -0700

    debugging logic

--- run_the_tests.R
+++ run_the_tests.R
@@ -3,44 +3,46 @@
 #
 
 run_the_tests <- function(Query.miner, Universe.miner, test.type, general.select, subset.by, subset.select, enrich, pval, adjpval){
+  browser()
+  intact_cat_result <- intact_main_result <- intact_sub_result <- chain_result <- allchains_result <- sub_total_carbon_result <- sub_total_DB_result <- sub_allchains_result <- NULL
   if(test.type=="Fisher"){
     #intact cat
-    if(general.select[1]==T){
+    if("cat" %in% general.select){ ############change these from T to name %in% general.select ###################
       if (enrich==T){
         intact_cat_result <- intact.fisher.enrich(Query.miner$intact$Category, Universe.miner$intact$Category,pval=pval,adjpval=adjpval)
       }else{
         intact_cat_result <- intact.fisher(Query.miner$intact$Category, Universe.miner$intact$Category)
       }}
     #intact main
-    if(general.select[2]==T){
+    if("main" %in% general.select){
       if (enrich==T){
         intact_main_result <- intact.fisher.enrich(Query.miner$intact$`Main class`, Universe.miner$intact$`Main class`,pval=pval,adjpval=adjpval)
       }else{
         intact_main_result <- intact.fisher(Query.miner$intact$`Main class`, Universe.miner$intact$`Main class`)
       }}
     #intact sub
-    if(general.select[3]==T){
+    if("sub" %in% general.select){
       if (enrich==T){
         intact_sub_result <- intact.fisher.enrich(Query.miner$intact$`Sub class`, Universe.miner$intact$`Sub class`,pval=pval,adjpval=adjpval)
       }else{
         intact_sub_result <- intact.fisher(Query.miner$intact$`Sub class`, Universe.miner$intact$`Sub class`)
       }}
     #chain
-    if(general.select[4]==T){
+    if("chains" %in% general.select){
       if (enrich==T){
         chain_result <- chain.fisher.enrich(Query.miner$chain, Universe.miner$chain,pval=pval,adjpval=adjpval)
       }else{
         chain_result <- chain.fisher(Query.miner$chain, Universe.miner$chain)
       }}
     #allchains
-    if(general.select[5]==T){
+    if("length" %in% general.select){
       if (enrich==T){
         allchains_result <- allchains.fisher.enrich(Query.miner$allchains, Universe.miner$allchains,pval=pval,adjpval=adjpval)
       }else{
         allchains_result <- allchains.fisher(Query.miner$allchains, Universe.miner$allchains)
       }}
     #sub total carbon
-    if(subset.select[1]==T){
+    if("total_carbon" %in% subset.select){
       if (subset.by=="category"){
         sub_total_carbon_result<- total.carbon.cat(Query.miner$intact,Universe.miner$intact, enrich=enrich,p=pval,adjp=adjpval)
       }
@@ -52,7 +54,7 @@ run_the_tests <- function(Query.miner, Universe.miner, test.type, general.select
       }
     }
     #sub total DB
-    if(subset.select[2]==T){
+    if("total_insaturation" %in% subset.select){
       if (subset.by=="category"){
         sub_total_DB_result<- total.DB.cat(Query.miner$intact,Universe.miner$intact, enrich=enrich,p=pval,adjp=adjpval)
       }
@@ -64,7 +66,7 @@ run_the_tests <- function(Query.miner, Universe.miner, test.type, general.select
       }
     }
     #sub allchains
-    if(subset.select[2]==T){
+    if("specific_chains" %in% subset.select){
       if (subset.by=="category"){
         sub_allchains_result<- allchains.cat(Query.miner$intact,Universe.miner$intact, enrich=enrich,p=pval,adjp=adjpval)
       }
@@ -79,42 +81,42 @@ run_the_tests <- function(Query.miner, Universe.miner, test.type, general.select
     #Binom
     if(test.type=="Binom"){
       #intact cat
-      if(general.select[1]==T){
+      if("cat" %in% general.select){
         if (enrich==T){
           intact_cat_result <- intact.binom.enrich(Query.miner$intact$Category, Universe.miner$intact$Category,pval=pval,adjpval=adjpval)
         }else{
           intact_cat_result <- intact.binom(Query.miner$intact$Category, Universe.miner$intact$Category)
         }}
       #intact main
-      if(general.select[2]==T){
+      if("main" %in% general.select){
         if (enrich==T){
           intact_main_result <- intact.binom.enrich(Query.miner$intact$`Main class`, Universe.miner$intact$`Main class`,pval=pval,adjpval=adjpval)
         }else{
           intact_main_result <- intact.binom(Query.miner$intact$`Main class`, Universe.miner$intact$`Main class`)
         }}
       #intact sub
-      if(general.select[3]==T){
+      if("sub" %in% general.select){
         if (enrich==T){
           intact_sub_result <- intact.binom.enrich(Query.miner$intact$`Sub class`, Universe.miner$intact$`Sub class`,pval=pval,adjpval=adjpval)
         }else{
           intact_sub_result <- intact.binom(Query.miner$intact$`Sub class`, Universe.miner$intact$`Sub class`)
         }}
       #chain
-      if(general.select[4]==T){
+      if("chains" %in% general.select){
         if (enrich==T){
           chain_result <- chain.binom.enrich(Query.miner$chain, Universe.miner$chain,pval=pval,adjpval=adjpval)
         }else{
           chain_result <- chain.binom(Query.miner$chain, Universe.miner$chain)
         }}
       #allchains
-      if(general.select[5]==T){
+      if("length" %in% general.select){
         if (enrich==T){
           allchains_result <- allchains.binom.enrich(Query.miner$allchains, Universe.miner$allchains,pval=pval,adjpval=adjpval)
         }else{
           allchains_result <- allchains.binom(Query.miner$allchains, Universe.miner$allchains)
         }}
       #sub total carbon
-      if(subset.select[1]==T){
+       if("total_carbon" %in% subset.select){
         if (subset.by=="category"){
           sub_total_carbon_result<- total.carbon.cat(Query.miner$intact,Universe.miner$intact, test= "Binom",enrich=enrich,p=pval,adjp=adjpval)
         }
@@ -126,19 +128,19 @@ run_the_tests <- function(Query.miner, Universe.miner, test.type, general.select
         }
       }
       #sub total DB
-      if(subset.select[2]==T){
-        if (subset.by=="category"){
-          sub_total_DB_result<- total.DB.cat(Query.miner$intact,Universe.miner$intact, test= "Binom",enrich=enrich,p=pval,adjp=adjpval)
+      if("total_insaturation" %in% subset.select){
+        if (subset.by == "category"){
+          sub_total_DB_result <- total.DB.cat(Query.miner$intact,Universe.miner$intact, test= "Binom",enrich=enrich,p=pval,adjp=adjpval)
         }
-        if (subset.by=="mainclass"){
-          sub_total_DB_result<- total.DB.main(Query.miner$intact,Universe.miner$intact, test= "Binom",enrich=enrich,p=pval,adjp=adjpval)
+        if (subset.by == "mainclass"){
+          sub_total_DB_result <- total.DB.main(Query.miner$intact,Universe.miner$intact, test= "Binom",enrich=enrich,p=pval,adjp=adjpval)
         }
-        if (subset.by=="subclass"){
-          sub_total_DB_result<- total.DB.sub(Query.miner$intact,Universe.miner$intact, test= "Binom",enrich=enrich,p=pval,adjp=adjpval)
+        if (subset.by == "subclass"){
+          sub_total_DB_result <- total.DB.sub(Query.miner$intact,Universe.miner$intact, test= "Binom",enrich=enrich,p=pval,adjp=adjpval)
         }
       }
       #sub allchains
-      if(subset.select[2]==T){
+      if("specific_chains" %in% subset.select){
         if (subset.by=="category"){
           sub_allchains_result<- allchains.cat(Query.miner$intact,Universe.miner$intact, test= "Binom",enrich=enrich,p=pval,adjp=adjpval)
         }
@@ -153,42 +155,42 @@ run_the_tests <- function(Query.miner, Universe.miner, test.type, general.select
       #Hyper
       if(test.type=="Hyper"){
         #intact cat
-        if(general.select[1]==T){
+        if("cat" %in% general.select){
           if (enrich==T){
             intact_cat_result <- intact.hyper.enrich(Query.miner$intact$Category, Universe.miner$intact$Category,pval=pval,adjpval=adjpval)
           }else{
             intact_cat_result <- intact.hyper(Query.miner$intact$Category, Universe.miner$intact$Category)
           }}
         #intact main
-        if(general.select[2]==T){
+        if("main" %in% general.select){
           if (enrich==T){
             intact_main_result <- intact.hyper.enrich(Query.miner$intact$`Main class`, Universe.miner$intact$`Main class`,pval=pval,adjpval=adjpval)
           }else{
             intact_main_result <- intact.hyper(Query.miner$intact$`Main class`, Universe.miner$intact$`Main class`)
           }}
         #intact sub
-        if(general.select[3]==T){
+        if("sub" %in% general.select){
           if (enrich==T){
             intact_sub_result <- intact.hyper.enrich(Query.miner$intact$`Sub class`, Universe.miner$intact$`Sub class`,pval=pval,adjpval=adjpval)
           }else{
             intact_sub_result <- intact.hyper(Query.miner$intact$`Sub class`, Universe.miner$intact$`Sub class`)
           }}
         #chain
-        if(general.select[4]==T){
+        if("chains" %in% general.select){
           if (enrich==T){
             chain_result <- chain.hyper.enrich(Query.miner$chain, Universe.miner$chain,pval=pval,adjpval=adjpval)
           }else{
             chain_result <- chain.hyper(Query.miner$chain, Universe.miner$chain)
           }}
         #allchains
-        if(general.select[5]==T){
+        if("length" %in% general.select){
           if (enrich==T){
             allchains_result <- allchains.hyper.enrich(Query.miner$allchains, Universe.miner$allchains,pval=pval,adjpval=adjpval)
           }else{
             allchains_result <- allchains.hyper(Query.miner$allchains, Universe.miner$allchains)
           }}
         #sub total carbon
-        if(subset.select[1]==T){
+         if("total_carbon" %in% subset.select){
           if (subset.by=="category"){
             sub_total_carbon_result<- total.carbon.cat(Query.miner$intact,Universe.miner$intact, test= "Hyper",enrich=enrich,p=pval,adjp=adjpval)
           }
@@ -200,7 +202,7 @@ run_the_tests <- function(Query.miner, Universe.miner, test.type, general.select
           }
         }
         #sub total DB
-        if(subset.select[2]==T){
+        if("total_insaturation" %in% subset.select){
           if (subset.by=="category"){
             sub_total_DB_result<- total.DB.cat(Query.miner$intact,Universe.miner$intact, test= "Hyper",enrich=enrich,p=pval,adjp=adjpval)
           }
@@ -212,7 +214,7 @@ run_the_tests <- function(Query.miner, Universe.miner, test.type, general.select
           }
         }
         #sub allchains
-        if(subset.select[2]==T){
+        if("specific_chains" %in% subset.select){
           if (subset.by=="category"){
             sub_allchains_result<- allchains.cat(Query.miner$intact,Universe.miner$intact, test= "Hyper",enrich=enrich,p=pval,adjp=adjpval)
           }
@@ -227,42 +229,42 @@ run_the_tests <- function(Query.miner, Universe.miner, test.type, general.select
         #EASE
         if(test.type=="EASE"){
           #intact cat
-          if(general.select[1]==T){
+          if("cat" %in% general.select){
             if (enrich==T){
               intact_cat_result <- intact.EASE.enrich(Query.miner$intact$Category, Universe.miner$intact$Category,pval=pval,adjpval=adjpval)
             }else{
               intact_cat_result <- intact.EASE(Query.miner$intact$Category, Universe.miner$intact$Category)
             }}
           #intact main
-          if(general.select[2]==T){
+          if("main" %in% general.select){
             if (enrich==T){
               intact_main_result <- intact.EASE.enrich(Query.miner$intact$`Main class`, Universe.miner$intact$`Main class`,pval=pval,adjpval=adjpval)
             }else{
               intact_main_result <- intact.EASE(Query.miner$intact$`Main class`, Universe.miner$intact$`Main class`)
             }}
           #intact sub
-          if(general.select[3]==T){
+          if("sub" %in% general.select){
             if (enrich==T){
               intact_sub_result <- intact.EASE.enrich(Query.miner$intact$`Sub class`, Universe.miner$intact$`Sub class`,pval=pval,adjpval=adjpval)
             }else{
               intact_sub_result <- intact.EASE(Query.miner$intact$`Sub class`, Universe.miner$intact$`Sub class`)
             }}
           #chain
-          if(general.select[4]==T){
+          if("chains" %in% general.select){
             if (enrich==T){
               chain_result <- chain.EASE.enrich(Query.miner$chain, Universe.miner$chain,pval=pval,adjpval=adjpval)
             }else{
               chain_result <- chain.EASE(Query.miner$chain, Universe.miner$chain)
             }}
           #allchains
-          if(general.select[5]==T){
+          if("length" %in% general.select){
             if (enrich==T){
               allchains_result <- allchains.EASE.enrich(Query.miner$allchains, Universe.miner$allchains,pval=pval,adjpval=adjpval)
             }else{
               allchains_result <- allchains.EASE(Query.miner$allchains, Universe.miner$allchains)
             }}
           #sub total carbon
-          if(subset.select[1]==T){
+           if("total_carbon" %in% subset.select){
             if (subset.by=="category"){
               sub_total_carbon_result<- total.carbon.cat(Query.miner$intact,Universe.miner$intact, test= "EASE",enrich=enrich,p=pval,adjp=adjpval)
             }
@@ -274,7 +276,7 @@ run_the_tests <- function(Query.miner, Universe.miner, test.type, general.select
             }
           }
           #sub total DB
-          if(subset.select[2]==T){
+          if("total_insaturation" %in% subset.select){
             if (subset.by=="category"){
               sub_total_DB_result<- total.DB.cat(Query.miner$intact,Universe.miner$intact, test= "EASE",enrich=enrich,p=pval,adjp=adjpval)
             }
@@ -286,7 +288,7 @@ run_the_tests <- function(Query.miner, Universe.miner, test.type, general.select
             }
           }
           #sub allchains
-          if(subset.select[2]==T){
+          if("specific_chains" %in% subset.select){
             if (subset.by=="category"){
               sub_allchains_result<- allchains.cat(Query.miner$intact,Universe.miner$intact, test= "EASE",enrich=enrich,p=pval,adjp=adjpval)
             }
@@ -314,7 +316,7 @@ run_the_tests <- function(Query.miner, Universe.miner, test.type, general.select
   colnames(global.output)<-c("Test.performed","Classifier","Count.query","Count.universe","%.query","%.universe","Pvalue","BHadjustPvalue","fold.change")
   
   options(warn=-1)
-  if(intact_cat_result!=F){
+  if(!is.null(intact_cat_result)){
     title.enrich<- paste("Category(",test.type,")",sep="")
     #display title
     title.enrich
@@ -323,7 +325,7 @@ run_the_tests <- function(Query.miner, Universe.miner, test.type, general.select
     global.output<-rbind(global.output,cbind(Test.performed=title.enrich,intact_cat_result))
   }
   
-  if(intact_main_result!=F){
+  if(!is.null(intact_main_result)){
     title.enrich<- paste("Main class(",test.type,")",sep="")
     #display title
     title.enrich
@@ -332,8 +334,8 @@ run_the_tests <- function(Query.miner, Universe.miner, test.type, general.select
     global.output<-rbind(global.output,cbind(Test.performed=title.enrich,intact_main_result))
   }
   
-  if(intact_sub_result!=F){
-    title.enrich<- paste("Sub class(",test.type,")",sep="")
+  if(!is.null(intact_sub_result)){
+    title.enrich <- paste("Sub class(",test.type,")",sep="")
     #display title
     title.enrich
     #display this on the right panel
@@ -341,7 +343,7 @@ run_the_tests <- function(Query.miner, Universe.miner, test.type, general.select
     global.output<-rbind(global.output,cbind(Test.performed=title.enrich,intact_sub_result))
   }
   
-  if(chain_result!=F){
+  if(!is.null(chain_result)){
     title.enrich<- paste("Chain(s) characteristics(",test.type,")",sep="")
     #display title
     title.enrich
@@ -350,7 +352,7 @@ run_the_tests <- function(Query.miner, Universe.miner, test.type, general.select
     global.output<-rbind(global.output,cbind(Test.performed=title.enrich,chain_result))
   }
   
-  if(allchains_result!=F){
+  if(!is.null(allchains_result)){
     title.enrich<- paste("Specific chain(",test.type,")",sep="")
     #display title
     title.enrich
@@ -359,7 +361,7 @@ run_the_tests <- function(Query.miner, Universe.miner, test.type, general.select
     global.output<-rbind(global.output,cbind(Test.performed=title.enrich,allchains_result))
   }
   
-  if(sub_total_carbon_result!=F){
+  if(!is.null(sub_total_carbon_result)){
     title.enrich<- paste("Total chain carbon by ",subset.by,"(",test.type,")",sep="")
     #display title
     title.enrich
@@ -368,7 +370,7 @@ run_the_tests <- function(Query.miner, Universe.miner, test.type, general.select
     global.output<-rbind(global.output,cbind(Test.performed=title.enrich, sub_total_carbon_result))
   }
   
-  if(sub_total_DB_result!=F){
+  if(!is.null(sub_total_DB_result)){
     title.enrich<- paste("Total number of DB by ",subset.by,"(",test.type,")",sep="")
     #display title
     title.enrich
@@ -377,7 +379,7 @@ run_the_tests <- function(Query.miner, Universe.miner, test.type, general.select
     global.output<-rbind(global.output,cbind(Test.performed=title.enrich, sub_total_DB_result))
   }
   
-  if(sub_allchains_result!=F){
+  if(!is.null(sub_allchains_result)){
     title.enrich<- paste("Specific chains by ",subset.by,"(",test.type,")",sep="")
     #display title
     title.enrich
